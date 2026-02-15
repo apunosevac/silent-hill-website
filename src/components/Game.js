9841f9b636bb09610a2459ecc086d5ca9a1a0ae3
apunosevac <aleksandar.punosevac@gmail.com>
2026-02-15 15:36:51 +0100

    Added search bar and changed text font

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,13 @@
-import { games } from '../data/data';
 import '../style/style.css';
 
-export default function Game() {
+export default function Game( {game} ) {
   return (
-    <div className='list-container'>
-      {games.map(game =>
-      <div className='game'>
-        <h1>{game.name}</h1>
-        <h2>{game.releaseYear}</h2>
-        <img alt={game.name} src={game.cover} className="coverArt"></img>
-        <h4>Protagonist: {game.protagonistName}</h4>
-        <p>Plot: {game.plot}</p>
-      </div>
-      )}
+    <div className='game'>
+      <h1>{game.name}</h1>
+      <h2>{game.releaseYear}</h2>
+      <img alt={game.name} src={game.cover} className="coverArt" />
+      <h4>Protagonist: {game.protagonistName}</h4>
+      <p>Plot: {game.plot}</p>
     </div>
   );
 }
--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import Game from "../components/Game";
 import Title from "../components/Title";
-import { gamesTitle } from "../data/data";
+import { gamesTitle, games } from "../data/data";
 
 export default function Games() {
-    return (
+
+  const [search, setSearch] = useState("");
+
+  const filteredGames = games.filter(game =>
+    game.name.toLowerCase().includes(search.toLowerCase()) ||
+    game.plot.toLowerCase().includes(search.toLowerCase()) ||
+    game.protagonistName.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
     <>
-        <Title title={gamesTitle}/>
-        <Game/>
+      <Title title={gamesTitle} />
+
+      <input
+        type="search"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <div className="list-container">
+        {filteredGames.map(game => (
+          <Game key={game.id} game={game} />
+        ))}
+      </div>
     </>
-    );
+  );
 }
